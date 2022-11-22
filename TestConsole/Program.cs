1ea8f6a536a5e8143b472dd609ded86a69e4c8f1
Paul Lesieur <plesieur@jpkeefehs.org>
2022-11-22 13:51:51 -0500

    Added Go, Look, Inventory and Take commands and all neccesasry support.

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using TextAdventureGameInputParser;
@@ -8,12 +11,21 @@ namespace TestConsole
 {
     internal class Program
     {
-        const bool debug = false;
+
+        const bool debug = true;
+        const int maxInventory = 5;
         private static void Main()
         {
 
             var parser = CreateParser();
+            var scene = CreateRooms();
+            int roomIndex = 0;
+            Room currenrRoom = scene[0];
             Sentence results;
+            List<string>inventory = new List<string>();
+
+
+            lookCmd(scene[roomIndex]);
             do
             {
                 Console.Write("\nParse what?> ");
@@ -22,7 +34,7 @@ namespace TestConsole
                 if (string.IsNullOrWhiteSpace(input))
                     return;
                 results = parser.Parse(input);
-                executeCommand(results, parser);
+                executeCommand(results, parser, ref roomIndex, scene, inventory);
             } while (true);
         }
 
@@ -30,7 +42,7 @@ namespace TestConsole
         {
             var parser = new Parser();
 
-            parser.AddVerbs("GO", "OPEN", "CLOSE", "GIVE", "SHOW", "LOOK", "INVENTORY", "GET", "TAKE", "DROP", "USE", "HELP", "QUIT");
+            parser.AddVerbs("GO", "OPEN", "CLOSE", "GIVE", "SHOW", "LOOK", "INVENTORY", "GET", "TAKE", "DROP", "USE", "EXAMINE", "HELP", "QUIT");
             parser.AddImportantFillers("TO", "ON", "IN");
             parser.AddUnimportantFillers("THE", "A", "AN", "AT");
             parser.AddNouns(
@@ -38,6 +50,8 @@ namespace TestConsole
                 "EAST",
                 "WEST",
                 "SOUTH",
+                "UP",
+                "DOWN",
                 "GREEN DOOR",
                 "BLUE DOOR",
                 "SKELETON KEY",
@@ -48,14 +62,82 @@ namespace TestConsole
             parser.Aliases.Add("GO EAST", "E", "EAST");
             parser.Aliases.Add("GO SOUTH", "S", "SOUTH");
             parser.Aliases.Add("GO WEST", "W", "WEST");
+            parser.Aliases.Add("GO UP", "U", "UP");
+            parser.Aliases.Add("GO DOWN", "D", "DOWN");
             parser.Aliases.Add("INVENTORY", "I", "INV");
             parser.Aliases.Add("HELP", "H", "?");
+            parser.Aliases.Add("QUIT", "Q", "EXIT");
 
             return parser;
         }
 
-        private static void executeCommand(Sentence results, Parser parser)
+        private static List<Room> CreateRooms()
         {
+            List<Room> rooms = new List<Room>();
+
+            rooms.Add(new Room()
+            {
+                Name = "Central Hall",
+                Description = "Main room of this test house.",
+                Items = new List<string> { },
+                Dir = new int[6] { 1, 2, 3, 4, -1, -1 }
+            });
+
+            rooms.Add(new Room()
+            {
+                Name = "North Room",
+                Description = "North chamber of the house.",
+                Items = new List<string> { "Gold Key" },
+                Dir = new int[6] { -1, -1, 0, -1, -1, -1 }
+            });
+
+            rooms.Add(new Room()
+            {
+                Name = "East Room",
+                Description = "East chamber of the house. The is a laddr going up and a staircase goig down.",
+                Items = new List<string> { },
+                Dir = new int[6] { -1, -1, -1, 0, 5, 6 }
+            });
+
+            rooms.Add(new Room()
+            {
+                Name = "South Room",
+                Description = "South chamber of the house. There is a Blue Door to the west and a Green Door to the east.",
+                Items = new List<string> { "Blue Door", "Green Door" },
+                Dir = new int[6] { 0, -1, -1, -1, -1, -1 }
+            });
+
+            rooms.Add(new Room()
+            {
+                Name = "West Room",
+                Description = "West chamber of the house",
+                Items = new List<string> { "Skeleton Key" },
+                Dir = new int[6] { -1, 0, -1, -1, -1, -1 }
+            });
+
+            rooms.Add(new Room()
+            {
+                Name = "Attack",
+                Description = "Attack of the house.  The roof is very low and you must stoop to walk through it.",
+                Items = new List<string> {  },
+                Dir = new int[6] { -1, -1, -1, -1, -1, 2 }
+            });
+
+            rooms.Add(new Room()
+            {
+                Name = "Cellar",
+                Description = "The cellar of the house.  There are empty wine racks along the walls.",
+                Items = new List<string> { },
+                Dir = new int[6] { -1, -1, -1, -1, 0, -1 }
+            });
+
+            return rooms;
+        }
+        private static void executeCommand(Sentence results, Parser parser, ref int roomIndex, List<Room> scene, List<string>inventory)
+        {
+            Room currentRoom;
+            currentRoom = scene[roomIndex];
+
             if (debug)
             {
                 Console.WriteLine(results);   //print debug info about parsed sentence
@@ -64,8 +146,10 @@ namespace TestConsole
             {
                 Console.WriteLine("Excuse Me?");   //Did not recognize command
             }
-            else
+            else if (results.Ambiguous)
             {
+                Console.WriteLine("Be more specfic with {0}", results.Word4.Value.ToLower());
+            } else { 
                 switch (results.Word1.Value)
                 {
                     case "HELP":
@@ -76,6 +160,51 @@ namespace TestConsole
                         Console.WriteLine("See Ya\n");
                         System.Environment.Exit(0);
                         break;
+                    case "GO":
+                        currentRoom.movement(results.Word4.Value,ref roomIndex);
+                        currentRoom = scene[roomIndex];
+                        lookCmd(scene[roomIndex]);
+                        break;
+                    case "LOOK":
+                        lookCmd(scene[roomIndex]);
+                        break;
+                    case "INVENTORY":
+                        if (inventory.Count == 0)
+                        {
+                            Console.WriteLine("You are not carrying anything");
+                        } else
+                        {
+                            Console.WriteLine("You are carrying");
+                            foreach(string item in inventory)
+                            {
+                                Console.WriteLine("  {0}", item);
+                            }
+                        }
+                        break;
+                    case "TAKE":
+                        if (inventory.Count == maxInventory)
+                        {
+                            Console.WriteLine("You cannot carry any more items");
+                            Console.WriteLine("Drop an item first");
+                        } else
+                        {
+                            string noun = (results.Word4.Value).ToLower();
+                            string adjective = (results.Word4.PrecedingAdjective.Value).ToLower();
+                            string item;
+                            if (adjective != null)
+                            {
+                                item = adjective + " " + noun;
+                            } else
+                            {
+                                item = noun;
+                            }
+
+                            Console.WriteLine("You have taken {0}", item);
+                            inventory.Add(item);
+                            scene[roomIndex].Items.Remove(item);
+                            
+                        }
+                        break;
                     default:
                         Console.WriteLine("Dunno");
                         break;
@@ -83,5 +212,18 @@ namespace TestConsole
             }
 
         }
+
+        private static void lookCmd(Room room)
+        {
+            Console.WriteLine("You are in the {0}", room.Name);
+            Console.WriteLine(room.Description);
+            if (room.Items.Count > 0)
+            {
+                Console.WriteLine("You see");
+                foreach (string item in room.Items) { Console.WriteLine("  {0}", item); }
+            }
+        }
+
+
     }
 }
---
+++ TestConsole/Rooms.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+/*    public class Direction
+    {
+        private int[] _dir;
+
+        public Direction(int[] direction)
+        {
+            _dir = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                _dir[i] = direction[i];
+            }
+        }
+
+        public int North() { return _dir[0]; }
+        public int East() { return _dir[1]; }
+        public int South() { return _dir[2]; }
+        public int West() { return _dir[3]; }
+        public int Up() { return _dir[4]; }
+        public int Down() { return _dir[5]; }
+    }
+*/
+    public class Room
+    {
+        private string _name;
+        private string _description;
+        private List<string> _items = new List<string>();
+        //        private Direction _travel;
+        private int[] _direction;
+
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Description { get { return _description; } set { _description = value; } }
+        public List<string> Items
+        {
+            get { return _items; }
+            set
+            {
+                foreach (string item in value)
+                {
+                    _items.Add(item.ToLower());
+                }
+            }
+        }
+
+        public int[] Dir {
+            get { return _direction; }
+            set {
+                _direction = value;
+            } 
+        }
+
+/*      
+        public int North() { return _direction[0]; }
+        public int East() { return _direction[1]; }
+        public int South() { return _direction[2]; }
+        public int West() { return _direction[3]; }
+        public int Up() { return _direction[4]; }
+        public int Down() { return _direction[5]; }
+*/
+        public void movement(string direction, ref int location)
+        {
+            List<string> directions = new List<string>() { "NORTH", "EAST", "SOUTH", "WEST", "UP", "DOWN" };
+
+            int dir = directions.IndexOf(direction);
+            location = this.Dir[dir];
+        }
+
+    }
+
+}
